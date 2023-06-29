2de7adf6c48df7f603da81c85e314c38c1e08da4
井柏然 <993967177@qq.com>
2023-06-29 19:45:29 +0800

    feat: cube demo

--- src/demo/fifth/1_1.vue
+++ src/demo/fifth/1_1.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <canvas id="ice-1_1" width="600" height="300"></canvas>
+  <canvas id="ice-1_1" width="600" height="400"></canvas>
 </template>
 
 <script setup lang="ts">
@@ -11,18 +11,16 @@ import {
   createProgram,
   createBuffer
 } from '@ice-webgl/utils'
-
-const xVal = ref(0)
-const yVal = ref(0)
+import { Matrix4 } from 'three'
 
 const vertexCode = `
   attribute vec4 a_Position;
   attribute vec4 a_Color;
   varying vec4 v_Color;
-  uniform vec4 u_Position;
+  uniform mat4 u_ModelMatrix;
 
   void main () {
-    gl_Position = a_Position + u_Position;
+    gl_Position = u_ModelMatrix * a_Position;
     v_Color= a_Color;
   }
 `
@@ -36,7 +34,7 @@ const fragmentCode = `
   }
 `
 
-let gl, a_Position, canvas, a_Color, program
+let gl, a_Position, canvas, a_Color, program, u_ModelMatrix
 
 const initGl = () => {
   gl = createGl('#ice-1_1')
@@ -48,42 +46,59 @@ const initGl = () => {
 
   a_Position = gl.getAttribLocation(program, 'a_Position')
   a_Color = gl.getAttribLocation(program, 'a_Color')
+  u_ModelMatrix = gl.getUniformLocation(program, 'u_ModelMatrix')
+  const matrix = new Matrix4()
+  // const radian = 45 * Math.PI / 180
+  // matrix.makeRotationY(radian)
+  // const matrix2 = new Matrix4()
+  // const radian2 = 45 * Math.PI / 180
+  // matrix2.makeRotationX(radian2)
+  // matrix2.multiply(matrix)
+  gl.uniformMatrix4fv(u_ModelMatrix, false, matrix.elements)
   const vertices = new Float32Array([
-    -.3, -.3,
-    0., .6,
-    .3, -.3,
+    .5, .5, .5,
+    -.5, .5, .5,
+    -.5, -.5, .5,
+    .5, -.5, .5,
+    .5, -.5, -.5,
+    .5, .5, -.5,
+    -.5, .5, -.5,
+    -.5, -.5, -.5,
   ])
   const colors = new Float32Array([
-    1., 0., 0., 1.,
-    0., 1., 0., 1.,
+    1., 1., 1., 1.,
+    1., 1., 0., 1.,
+    1., 1., 1., 1.,
+    1., 0., 1., 1.,
+    0., 1., 1., 1.,
     0., 0., 1., 1.,
+    1., 1., 0., 1.,
+    0., 1., 1., 1.,
   ])
 
+  const indices = new Uint8Array([
+      0, 1, 2, 0, 2, 3,
+      0, 3, 4, 0, 4, 5,
+      0, 5, 6, 0, 6, 1,
+      1, 6, 7, 1, 7, 2,
+      7, 4, 3, 7, 3, 2,
+      4, 7, 6, 4, 6, 5
+  ])
+
+  const indexBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
+
   // 顶点坐标
-  createBuffer(gl, gl.ARRAY_BUFFER, vertices, a_Position, 2)
+  createBuffer(gl, gl.ARRAY_BUFFER, vertices, a_Position, 3)
   // 颜色值
   createBuffer(gl, gl.ARRAY_BUFFER, colors, a_Color, 4)
 
   gl.clearColor(0., 0., 0., .9)
   gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
+  // gl.drawArrays(gl.TRIANGLES, 0, 3)
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_BYTE, 0)
 }
-
-watch(xVal, x => {
-  const u_Position = gl.getUniformLocation(program, 'u_Position')
-  const xMoveVertices = new Float32Array([x, yVal.value, 0., 0.])
-  gl.uniform4fv(u_Position, xMoveVertices)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
-})
-
-watch(yVal, y => {
-  const u_Position = gl.getUniformLocation(program, 'u_Position')
-  const xMoveVertices = new Float32Array([xVal.value, y, 0., 0.])
-  gl.uniform4fv(u_Position, xMoveVertices)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
-})
 
 onMounted(() => {
   initGl()
