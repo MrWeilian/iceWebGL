5cd2ee2356cace2fb132d1bf5f43267b5e81bff2
井柏然 <993967177@qq.com>
2023-07-03 20:17:09 +0800

    feat: hook

--- src/demo/fifth/1_1.vue
+++ src/demo/fifth/1_1.vue
@@ -4,7 +4,7 @@
       width="640"
       height="500"
       @mousedown="mousedown"
-      @mousemove="mousemove"
+      @mousemove="mousemove($event, gl, u_ModelMatrix, indices)"
       @mouseleave="mouseleave"
       @mouseup="mouseup"
   />
@@ -19,8 +19,7 @@ import {
   createBuffer
 } from '@ice-webgl/utils'
 import { Matrix4 } from 'three'
-
-const canDrag = ref(false)
+import { useMouseMatrixRotate } from '@ice-webgl/utils'
 
 const vertexCode = `
   attribute vec4 a_Position;
@@ -47,8 +46,13 @@ let gl, a_Position, canvas, a_Color, program, u_ModelMatrix, indices
 
 let baseRotateX = 0
 let baseRotateY = 0
-let mousedownX = 0
-let mousedownY = 0
+
+const {
+  mousemove,
+  mousedown,
+  mouseleave,
+  mouseup
+} = useMouseMatrixRotate(baseRotateX, baseRotateY)
 
 const initGl = () => {
   gl = createGl('#ice-1_1')
@@ -106,41 +110,6 @@ const initGl = () => {
   gl.clearColor(0., 0., 0., .9)
   gl.clear(gl.COLOR_BUFFER_BIT)
   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0)
-}
-
-const mousemove = e => {
-  if (!canDrag.value) return
-  const moveX = (mousedownX - e.offsetX + baseRotateX) % 360
-  const moveY = (mousedownY - e.offsetY + baseRotateY) % 360
-  console.log('moveX', moveX)
-  console.log('moveY', moveY)
-  const matrixX = new Matrix4()
-  const matrixY = new Matrix4()
-  const radianX = moveX * Math.PI / 180
-  const radianY = moveY * Math.PI / 180
-  matrixX.makeRotationY(radianX)
-  matrixY.makeRotationX(radianY)
-  matrixX.multiply(matrixY)
-  gl.uniformMatrix4fv(u_ModelMatrix, false, matrixX.elements)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0)
-
-}
-
-const mousedown = e => {
-  canDrag.value = true
-  mousedownX = e.offsetX
-  mousedownY = e.offsetY
-}
-
-const mouseleave = () => {
-  canDrag.value = false
-}
-
-const mouseup = e => {
-  canDrag.value = false
-  baseRotateX = (mousedownX - e.offsetX + baseRotateX) % 360
-  baseRotateY = (mousedownY - e.offsetY + baseRotateY) % 360
 }
 
 onMounted(() => {
---
+++ src/utils/useMouseMatrixRotate.ts
@@ -0,0 +1,50 @@
+import { ref } from 'vue'
+import { Matrix4 } from 'three'
+
+export function useMouseMatrixRotate (baseRotateX = 0, baseRotateY = 0) {
+  let mousedownX = 0
+  let mousedownY = 0
+
+  const canDrag = ref(false)
+
+  const mousemove = (e, gl, u_ModelMatrix, indices) => {
+    if (!canDrag.value) return
+    const moveX = (mousedownX - e.offsetX + baseRotateX) % 360
+    const moveY = (mousedownY - e.offsetY + baseRotateY) % 360
+    console.log('moveX', moveX)
+    console.log('moveY', moveY)
+    const matrixX = new Matrix4()
+    const matrixY = new Matrix4()
+    const radianX = moveX * Math.PI / 180
+    const radianY = moveY * Math.PI / 180
+    matrixX.makeRotationY(radianX)
+    matrixY.makeRotationX(radianY)
+    matrixX.multiply(matrixY)
+    gl.uniformMatrix4fv(u_ModelMatrix, false, matrixX.elements)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0)
+  }
+
+  const mousedown = e => {
+    canDrag.value = true
+    mousedownX = e.offsetX
+    mousedownY = e.offsetY
+  }
+
+  const mouseleave = () => {
+    canDrag.value = false
+  }
+
+  const mouseup = e => {
+    canDrag.value = false
+    baseRotateX = (mousedownX - e.offsetX + baseRotateX) % 360
+    baseRotateY = (mousedownY - e.offsetY + baseRotateY) % 360
+  }
+
+  return {
+    mousemove,
+    mousedown,
+    mouseleave,
+    mouseup
+  }
+}
