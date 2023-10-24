cf398636a320649ca07991a3c9c9672735eb8292
井柏然 <993967177@qq.com>
2023-10-24 18:16:01 +0800

    feat: 逐顶点光照效果的问题

--- src/demo/sixth/index.ts
+++ src/demo/sixth/index.ts
@@ -3,6 +3,7 @@ import Sixth2_1 from './2_1.vue'
 import Sixth3_1 from './3_1.vue'
 import Sixth4_1 from './4_1.vue'
 import Sixth4_2 from './4_2.vue'
+import Sixth5_1 from './5_1.vue'
 
 export const sixthComponents = [
   Sixth1_1,
@@ -10,4 +11,5 @@ export const sixthComponents = [
   Sixth3_1,
   Sixth4_1,
   Sixth4_2,
+  Sixth5_1,
 ]
