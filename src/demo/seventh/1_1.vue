77bf28b74edf800a2ea81a7492a5d196032dc36b
井柏然 <993967177@qq.com>
2024-03-25 00:12:13 +0800

    feat: preitter config

--- src/demo/seventh/1_1.vue
+++ src/demo/seventh/1_1.vue
@@ -3,20 +3,20 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue';
 import {
   createGl,
   createShader,
   createProgram,
-  createBuffer
-} from '@ice-webgl/utils'
-import { Matrix4, Vector3, Vector4 } from '../cuon-matrix'
+  createBuffer,
+} from '@ice-webgl/utils';
+import { Matrix4, Vector3, Vector4 } from '../cuon-matrix';
 import axios from 'axios';
-import parseWFObj from 'wavefront-obj-parser'
+import parseWFObj from 'wavefront-obj-parser';
 
-const xLight = ref(3)
-const yLight = ref(2)
-const zLight = ref(1)
+const xLight = ref(3);
+const yLight = ref(2);
+const zLight = ref(1);
 
 const vertexCode = `
   attribute vec4 a_Position;
@@ -37,7 +37,7 @@ const vertexCode = `
     // v_Color= vec4(colorRes, a_Color.a);
     v_Color= vec4(0., 0., 0., 1.);
   }
-`
+`;
 
 const fragmentCode = `
   precision mediump float;
@@ -46,75 +46,88 @@ const fragmentCode = `
   void main () {
     gl_FragColor = v_Color;
   }
-`
+`;
 
-let gl, a_Position, canvas, a_Color, a_Normal, program, u_MvpMatrix, u_LightColor, u_LightDirection, indices
+let gl,
+  a_Position,
+  canvas,
+  a_Color,
+  a_Normal,
+  program,
+  u_MvpMatrix,
+  u_LightColor,
+  u_LightDirection,
+  indices;
 
 const initGl = ({ vertexPositions, vertexPositionIndices, vertexNormals }) => {
-  gl = createGl('#ice-1_1')
+  gl = createGl('#ice-1_1');
 
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexCode)
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentCode)
+  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexCode);
+  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentCode);
 
-  program = createProgram(gl, vertexShader, fragmentShader)
+  program = createProgram(gl, vertexShader, fragmentShader);
 
-  a_Position = gl.getAttribLocation(program, 'a_Position')
-  a_Color = gl.getAttribLocation(program, 'a_Color')
-  a_Normal = gl.getAttribLocation(program, 'a_Normal')
-  u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix')
-  u_LightColor = gl.getUniformLocation(program, 'u_LightColor')
-  u_LightDirection = gl.getUniformLocation(program, 'u_LightDirection')
+  a_Position = gl.getAttribLocation(program, 'a_Position');
+  a_Color = gl.getAttribLocation(program, 'a_Color');
+  a_Normal = gl.getAttribLocation(program, 'a_Normal');
+  u_MvpMatrix = gl.getUniformLocation(program, 'u_MvpMatrix');
+  u_LightColor = gl.getUniformLocation(program, 'u_LightColor');
+  u_LightDirection = gl.getUniformLocation(program, 'u_LightDirection');
 
-  const matrix = new Matrix4()
-  matrix
-    .perspective(50, 1, 1, 100)
-    .lookAt(1.3, .5, 1.3, 0, 0, 0, 0, 1, 0)
-  gl.uniformMatrix4fv(u_MvpMatrix, false, matrix.elements)
+  const matrix = new Matrix4();
+  matrix.perspective(50, 1, 1, 100).lookAt(1.3, 0.5, 1.3, 0, 0, 0, 0, 1, 0);
+  gl.uniformMatrix4fv(u_MvpMatrix, false, matrix.elements);
 
-  const vertices = new Float32Array(vertexPositions)
+  const vertices = new Float32Array(vertexPositions);
 
-  indices = new Uint16Array(vertexPositionIndices)
+  indices = new Uint16Array(vertexPositionIndices);
 
-  const lightColor = new Vector4(1.0, 1.0, 1.0, 1.0)
-  gl.uniform4fv(u_LightColor, lightColor.elements)
+  const lightColor = new Vector4(1.0, 1.0, 1.0, 1.0);
+  gl.uniform4fv(u_LightColor, lightColor.elements);
 
-  const lightDirection = new Vector3(xLight.value, yLight.value, zLight.value)
-  gl.uniform3fv(u_LightDirection, lightDirection.elements)
+  const lightDirection = new Vector3(xLight.value, yLight.value, zLight.value);
+  gl.uniform3fv(u_LightDirection, lightDirection.elements);
 
-  const indexBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
-  gl.enable(gl.DEPTH_TEST)
+  const indexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+  gl.enable(gl.DEPTH_TEST);
 
   // 顶点坐标
-  createBuffer(gl, gl.ARRAY_BUFFER, vertices, a_Position, 3)
+  createBuffer(gl, gl.ARRAY_BUFFER, vertices, a_Position, 3);
   // 颜色值
   // createBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(), a_Color, 3)
   // 点的法向量
-  createBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(vertexNormals), a_Normal, 3)
+  createBuffer(
+    gl,
+    gl.ARRAY_BUFFER,
+    new Float32Array(vertexNormals),
+    a_Normal,
+    3,
+  );
   console.log('indices.length', indices.length);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0)
-}
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+};
 
 const readOBJFile = async () => {
-  const res = await axios.get('/models/fan/Colling Fan.obj')
-  const parseObject = parseWFObj(res.data)
-  console.log('parseObject', parseObject)
-  initGl(parseObject)
-}
+  const res = await axios.get('/models/fan/Colling Fan.obj');
+  const parseObject = parseWFObj(res.data);
+  console.log('parseObject', parseObject);
+  initGl(parseObject);
+};
 
 onMounted(() => {
-  readOBJFile()
-})
+  readOBJFile();
+});
 </script>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'Seventh1_1'
-})
+  name: 'Seventh1_1',
+});
 </script>
 
 <style lang="scss" scoped>
--- src/demo/seventh/2_1.vue
+++ src/demo/seventh/2_1.vue
@@ -1,51 +1,54 @@
 <template>
   <div
-      class="canvas-wrapper"
-      ref="canvasRef"
-      @mousedown="mousedown"
-      @mouseleave="mouseleave"
-      @mousemove="mousemove"
-      @mouseup="mouseup"
+    class="canvas-wrapper"
+    ref="canvasRef"
+    @mousedown="mousedown"
+    @mouseleave="mouseleave"
+    @mousemove="mousemove"
+    @mouseup="mouseup"
   />
 </template>
 
 <script setup lang="ts">
-import * as THREE from 'three';
-import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
-import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
-import { onMounted, ref } from 'vue';
-import { useMouseCamera } from '@ice-webgl/utils/useMouseCamera';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import { onMounted, ref } from "vue";
+import { useMouseCamera } from "@ice-webgl/utils/useMouseCamera";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const canvasRef = ref(null);
-const cameraPosition = ref([1, 1, 6])
-const isDrag = ref(false)
+const isDrag = ref(false);
 
-let camera, renderer, scene, controls
+let camera, renderer, scene, controls;
 
 const downOption = () => {
   isDrag.value = true;
-}
+};
 
 const moveOption = () => {
   if (!isDrag.value) return;
   controls.update();
   renderer.render(scene, camera);
-}
+};
 
 const upOption = () => {
-  isDrag.value = false
-}
+  isDrag.value = false;
+};
 
-const {
-  mousedown,
-  mousemove,
-  mouseleave,
-  mouseup
-} = useMouseCamera({ moveOption, upOption, downOption })
+const { mousedown, mousemove, mouseleave, mouseup } = useMouseCamera({
+  moveOption,
+  upOption,
+  downOption,
+});
 
 const initFn = () => {
-  camera = new THREE.PerspectiveCamera(60, canvasRef.value.offsetWidth / canvasRef.value.offsetHeight, 1, 100);
+  camera = new THREE.PerspectiveCamera(
+    60,
+    canvasRef.value.offsetWidth / canvasRef.value.offsetHeight,
+    1,
+    100,
+  );
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer();
   let ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
@@ -60,44 +63,44 @@ const initFn = () => {
   renderer.setSize(canvasRef.value.offsetWidth, canvasRef.value.offsetHeight);
   canvasRef.value.appendChild(renderer.domElement);
   // console.log('loader', loader);
-  MTLloader.load('/models/car/Car/Car_Obj.mtl', function (materials) {
-    loader.setMaterials(materials)
+  MTLloader.load("/models/car/Car/Car_Obj.mtl", function (materials) {
+    loader.setMaterials(materials);
     loader.load(
-        '/models/car/Car/Car Obj.obj',
+      "/models/car/Car/Car Obj.obj",
 
-        // onLoad回调
-        // Here the loaded data is assumed to be an object
-        function (obj) {
-          scene.add(obj);
-          controls = new OrbitControls(camera, renderer.domElement);
-          controls.update();
-          renderer.render(scene, camera);
-        },
+      // onLoad回调
+      // Here the loaded data is assumed to be an object
+      function (obj) {
+        scene.add(obj);
+        controls = new OrbitControls(camera, renderer.domElement);
+        controls.update();
+        renderer.render(scene, camera);
+      },
 
-        // onProgress回调
-        function (xhr) {
-          console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        },
+      // onProgress回调
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
 
-        // onError回调
-        function (err) {
-          console.error('An error happened');
-        }
+      // onError回调
+      function (err) {
+        console.error("An error happened");
+      },
     );
-  })
-}
+  });
+};
 
 onMounted(() => {
   initFn();
-})
+});
 </script>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Seventh2_1'
-})
+  name: "Seventh2_1",
+});
 </script>
 
 <style lang="scss" scoped>
