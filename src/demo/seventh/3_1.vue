7918b5f1cae18b7c2cfe26bb62278b03166123f0
井柏然 <993967177@qq.com>
2024-03-31 23:30:16 +0800

    feat: 加载模型文件

---
+++ src/demo/seventh/3_1.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="ice-3_1" />
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const vertexCode = `
+  attribute vec4 a_Position;
+
+  void main () {
+    gl_Poisition = a_Position;
+  }
+`;
+
+const fragmentCode = `
+  precision mediump float;
+  varying vec4 v_Color;
+
+  void main () {
+    gl_FragColor = v_Color;
+  }
+`;
+
+const loadOBJ = async () => {
+  const result = await axios.get('/models/car/Car/Car Obj.obj');
+  const obj = result.data;
+  console.log(11, obj);
+};
+
+const initRender = () => {
+  
+}
+
+onMounted(() => {
+  loadOBJ();
+});
+</script>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Seventh3_1',
+});
+</script>
+
+<style lang="scss" scoped>
+#ice-3_1 {
+  margin: 0 auto;
+  width: 100%;
+  height: 400px;
+}
+</style>
--- src/demo/seventh/index.ts
+++ src/demo/seventh/index.ts
@@ -1,7 +1,9 @@
 import Seventh1_1 from './1_1.vue'
 import Seventh2_1 from './2_1.vue'
+import Seventh3_1 from './3_1.vue'
 
 export const seventhComponents = [
   Seventh1_1,
   Seventh2_1,
+  Seventh3_1
 ]
